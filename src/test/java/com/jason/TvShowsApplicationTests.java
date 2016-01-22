52240ddc2fd7e17490355f1b4291757016eef153
jason wang <519875872@qq.com>
2016-01-22 10:31:09 +0800

    add mvcTest

--- src/test/java/com/jason/TvShowsApplicationTests.java
+++ src/test/java/com/jason/TvShowsApplicationTests.java
@@ -1,18 +1,36 @@
 package com.jason;
 
+import com.jason.controller.HelloController;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = TvShowsApplication.class)
 @WebAppConfiguration
 public class TvShowsApplicationTests {
+    private MockMvc mvc;
 
-	@Test
-	public void contextLoads() {
-	}
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+    }
 
+    @Test
+    public void get_hello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("hello controller")));
+    }
 }
