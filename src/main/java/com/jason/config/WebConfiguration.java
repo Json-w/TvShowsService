66c1559c8c58495909f1934cae2dbec02b78e3d6
wang pei <519875872@qq.com>
2016-02-14 16:02:48 +0800

    add customer static resource path

--- src/main/java/com/jason/config/WebConfiguration.java
+++ src/main/java/com/jason/config/WebConfiguration.java
@@ -4,6 +4,7 @@ import com.jason.interceptor.AccessInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
@@ -15,6 +16,14 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(getAccessInterceptor()).excludePathPatterns("/user/login").excludePathPatterns("/user/register");
+        registry.addInterceptor(getAccessInterceptor()).excludePathPatterns("/user/login")
+                .excludePathPatterns("/user/register")
+                .excludePathPatterns("/pics/**");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/pics/**").addResourceLocations("file:/home/jason/python/graduateDesign/tvShowSpider/pics/");
+        super.addResourceHandlers(registry);
     }
 }
--- src/main/java/com/jason/controller/TvShowController.java
+++ src/main/java/com/jason/controller/TvShowController.java
@@ -1,5 +1,6 @@
 package com.jason.controller;
 
+import com.jason.model.ChooseTvShow;
 import com.jason.model.ResponseData;
 import com.jason.model.Status;
 import com.jason.model.TvShow;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,10 +20,24 @@ public class TvShowController {
     private TvShowService tvShowService;
 
     @RequestMapping("/listLatest")
-    private ResponseData listLatestTvShows(int page, int size) {
+    public ResponseData listLatestTvShows(int page, int size) {
         Pageable pageRequest = new PageRequest(page, size);
         ResponseData responseData = new ResponseData();
         Page<TvShow> findResult = tvShowService.findLatestTvShows(pageRequest);
+        if (null != findResult) {
+            responseData.setStatus(Status.SUCCESS);
+            responseData.setData(findResult);
+        } else {
+            responseData.setStatus(Status.FAILURE);
+        }
+        return responseData;
+    }
+
+    @RequestMapping("/listChooseTvShows/{userId}")
+    public ResponseData listChooseTvShowsByUserId(@PathVariable int userId, int page, int size) {
+        PageRequest pageRequest = new PageRequest(page,size);
+        ResponseData responseData = new ResponseData();
+        Page<ChooseTvShow> findResult = tvShowService.findChooseTvShowByUserId(pageRequest, userId);
         if (null != findResult) {
             responseData.setStatus(Status.SUCCESS);
             responseData.setData(findResult);
